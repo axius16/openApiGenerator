883a018056b71cec3d59eae7a5d89d44022d7c16
Naza <nazareno.albarracin@recargapay.com>
2025-03-26 10:38:48 -0300

    Test

--- src/main/java/com/recarga/payments/rest/controller/PingController.java
+++ src/main/java/com/recarga/payments/rest/controller/PingController.java
@@ -4,10 +4,10 @@ package com.recarga.payments.rest.controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/ping")
+@RequestMapping("/asdasdasd")
 public class PingController {
     @GetMapping
     public String ping() {
-        return "pong";
+        return "asdasdasd";
     }
 }
